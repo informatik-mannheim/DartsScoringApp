6c5fec3bccec6b1fa5a22cc97b041e8bc0afb39f
oliverhummel <o.hummel@hs-mannheim.de>
2019-01-08 22:52:46 +0100

    Initial commit of main Java class

---
+++ src/de/hsma/informatik/pr1/darts/MainApp.java
@@ -0,0 +1,19 @@
+package de.hsma.informatik.pr1.darts;
+
+/**
+ * DartsScoringApp
+ * An (initially) simple Java for for counting scores in Dart games.
+ * 
+ * Intended as a manageable real world example in Java programming 
+ * for first semester computer science students at Hochschule Mannheim.
+ * 
+ * @author Oliver Hummel (https://www.informatik.hs-mannheim.de/hummel.html)
+ *
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Darts!");
+	}
+
+}
