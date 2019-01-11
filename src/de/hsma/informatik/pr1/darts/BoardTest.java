ec1946088976af44c666d6e02be53be671faa876
oliverhummel <o.hummel@hs-mannheim.de>
2019-01-11 10:19:52 +0100

    Extended tests for input parsing.

--- src/de/hsma/informatik/pr1/darts/BoardTest.java
+++ src/de/hsma/informatik/pr1/darts/BoardTest.java
@@ -16,6 +16,33 @@ class BoardTest {
 		assertEquals(4, parseInput("d2"));
 		assertEquals(22, parseInput("D11"));
 		assertEquals(2, parseInput(" d1 "));
+		
+		assertEquals(50, parseInput("50"));
+	}
+	
+	@Test
+	void testInputParsingForBullAndBullsEye() {
+		assertEquals(25, parseInput("bl"));
+		assertEquals(25, parseInput("BL"));
+		assertEquals(50, parseInput("be"));
+		assertEquals(50, parseInput("BE"));
+	}
+	
+	@Test
+	void testInputParsingForSingleScores() {
+		assertEquals(25, parseInput("25"));
+
+		for (int i = 0; i < 1; i++) {
+			assertEquals(i, parseInput("" + i));
+		}
+	}
+	
+	@Test
+	void testInputParsingForNilScores() {
+		assertEquals(0, parseInput("-"));
+		assertEquals(0, parseInput("--"));
+		assertEquals(0, parseInput("ms"));
+		assertEquals(0, parseInput("bo"));
 	}
 
 }
